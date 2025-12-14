f760024afae6be1a8cf6cd02d91246c6c6714db9
ChakradharReddy01 <111549918+ChakradharReddy01@users.noreply.github.com>
2025-12-14 22:08:43 +0530

    Create Home component with basic layout

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{
+      fontFamily: "Arial",
+      textAlign: "center",
+      marginTop: "20%"
+    }}>
+      <h1>Hello World 🌍</h1>
+      <h2>Next.js hosted on Render</h2>
+    </main>
+  );
+}
